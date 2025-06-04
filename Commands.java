0116bf53ceca51af724dd01a628e3cfb10c66b4b
FunkyTurtl3 <lasse2008s@icloud.com>
2025-06-04 18:32:18 +0200

    Initial Commit for this project

--- Commands.java
+++ Commands.java
@@ -4,10 +4,10 @@
  * In dieser Aufzählung sind alle verfügbaren Commands (also das erste Wort einer Eingabe) gespeichert
  */
 public enum Commands {
-    ZU,
-    NIMM,
-    GIB,
-    INVALIDINPUT,
-    STARTE,
-    INFO;
+    ZU, //Zum Wechseln von Räumen
+    NIMM, //Zum Aufnehmen von Gegenständen
+    GIB, //Zum Ablegen von Gegenständen
+    INVALIDINPUT, //Zum Ausgeben von ungültigen Eingaben
+    STARTE, //Zum (Neu-)Starten des Spiels
+    INFO; //Zum Ausgeben einer Spielinfo
 }
